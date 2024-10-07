6f13e0fc27e6bb2534352487215f380f2ecfc333
A-Aldo <aldo_irianto_30rpl@student.smktelkom-mlg.sch.id>
2024-10-07 16:00:24 +0700

    nambah lagu

--- reveal.js
+++ reveal.js
@@ -1,17 +1,22 @@
-const revealElements = document.querySelectorAll("[data-reveal]")
+const revealElements = document.querySelectorAll("[data-reveal]");
 
-const scrollReveal =  function () {
-    for (let i = 0, len = revealElements.length; i < len; i++) {
+const scrollReveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementsOnScreen =
+      revealElements[i].getBoundingClientRect().top < window.innerHeight;
 
-        const isElementsOnScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight;
-       
-        if(isElementsOnScreen) {
-            revealElements[i].classList.add("revealed")
-        } else {
-            revealElements[i].classList.remove("revealed")
-        }
-    }   
+    if (isElementsOnScreen) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+};
 
-}
+window.onload = function () {
+  const audio = document.getElementById("myAudio");
+  audio.currentTime = 85; 
+  audio.play();
+};
 
 window.addEventListener("scroll", scrollReveal);
